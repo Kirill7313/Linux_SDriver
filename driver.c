98d2bf3524ff475cd464e4af8a6cfa2fc3ffdbb8
Kirill7313 <73630738+Kirill7313@users.noreply.github.com>
2024-06-21 14:52:49 +0400

    Update driver.c

--- driver.c
+++ driver.c
@@ -1,15 +1,15 @@
 #include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
 MODULE_LICENSE("GPLv3");
 MODULE_AUTHOR("Kirill7313");
-int in(void)
-{
-    printk(KERN_INFO "Hello, I'm Linux Driver!\n");
+MODULE_DESCRIPTION("The simple GNU/Linux driver!");
+static int __init in(void){
+    pr_info("Hello, I'm Linux Driver!\n");
     return 0;
 }
-void ex(void)
-{
-    printk(KERN_INFO "Goodbye, Linux!\n");
+static void __exit ex(void){
+    pr_info("Goodbye, Linux!\n");
 }
 module_init(in);
 module_exit(ex);
